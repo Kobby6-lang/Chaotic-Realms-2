fd4c816ac10c7545f59d6113117f67345b2ceef1
Kobby6-lang <koseitutu43@gmail.com>
2024-11-11 18:16:50 +0000

    ?

--- Assets/Enemies/Animations/EnemyScripts/ChaseState.cs
+++ Assets/Enemies/Animations/EnemyScripts/ChaseState.cs
@@ -21,7 +21,7 @@ public class ChaseState : StateMachineBehaviour
     {
         agent.SetDestination(player.position);
         float distance = Vector3.Distance(player.position, animator.transform.position);
-        if (distance > .001)
+        if (distance > .01)
             animator.SetBool("isChasing", false);
         if (distance < 2.5f)
             animator.SetBool("isAttacking", true);

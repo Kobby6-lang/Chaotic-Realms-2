176fbac002d929968541ee6f3e48045fce1c9267
Kobby6-lang <koseitutu43@gmail.com>
2024-11-19 16:44:01 +0000

    Ai Redo

--- Assets/Scripts/StateMachine/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachine/PlayerStateMachine.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class PlayerStateMachine : MonoBehaviour
 {
     // declare reference variables
